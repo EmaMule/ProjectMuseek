89d9bf4ab9a2040291a35e5305bfc38a5b3df982
palu001 <100961049+palu001@users.noreply.github.com>
2023-02-03 23:58:04 +0100

    FORZA KYLIE JENNER

--- ProgettinoSimoneP/Server.py
+++ ProgettinoSimoneP/Server.py
@@ -2,30 +2,53 @@ import socket
 import threading
 import time
 import defFunctions
+import getInformation
 
+wsem=threading.Semaphore(1)
+y=threading.Semaphore(1)
+readcount=0
 def send_to_client(conn):
+    global wsem,y,readcount
     db = defFunctions.collegamento()
     cursor=db.cursor()
     sql="SELECT * FROM clienti"
+    y.acquire()
+    readcount+=1
+    if readcount==1:
+        wsem.acquire()
+    y.release()
     cursor.execute(sql)
     result=cursor.fetchall()
-    nibbio = ";"
+    y.acquire()
+    readcount-=1
+    if readcount==0:
+        wsem.release()
+    y.release()
+    separator = ";"
     for riga in result:
-        time.sleep(2)
         conn.send(riga[0].encode())
-        conn.send(nibbio.encode())
+        conn.send(separator.encode())
         conn.send(riga[1].encode())
-        conn.send(nibbio.encode())
+        conn.send(separator.encode())
         conn.send(riga[2].encode())
-        conn.send(nibbio.encode())
+        conn.send(separator.encode())
+        conn.send(str(riga[3]).encode())
+        conn.send(separator.encode())
     conn.close()
-  
-    
+def informationWrapper():
+    global wsem
+    while(True):
+        wsem.acquire()
+        print("Sto aggiornando")
+        getInformation.getInf()
+        wsem.release()
+        print("Ho finito di aggiornare")
+        time.sleep(3600)
 
-
+thread_inf=threading.Thread(target=informationWrapper)
+thread_inf.start()
 port = 63222
 host = '127.0.0.1'
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((host,port))
 sock.listen(5)
--- ProgettinoSimoneP/database.py
+++ ProgettinoSimoneP/database.py
@@ -3,7 +3,7 @@ import mysql.connector
 db=mysql.connector.connect(
     host="localhost",
     user="root",
-    password="Ganonnino",
+    password="password",
     database="progettino" #inizialmente no
 )
 
@@ -14,5 +14,8 @@ cursor=db.cursor()
 #cursor.execute("SHOW DATABASES")
 #sql="INSERT INTO clienti(Titolo,Link,LinkFoto) VALUES (%s,%s,%s)"
 #cursor.execute(sql,values)
+#cursor.execute("ALTER TABLE clienti ADD COLUMN Data date;")
+cursor.execute("ALTER TABLE clienti ORDER BY Data DESC;")
 db.commit()
+
 
