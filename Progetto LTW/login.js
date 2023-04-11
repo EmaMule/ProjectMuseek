f3c2af8fc4b799ef42c46cfd2d151c5e46b099da
EmaMule <100977128+EmaMule@users.noreply.github.com>
2023-04-11 23:53:37 +0200

    EVVAIIII

---
+++ Progetto LTW/login.js
@@ -0,0 +1,84 @@
+function mostranascondi() {
+  var form = document.querySelector(".login-box");
+  form.style.display = "none";
+  var passwords = document.querySelectorAll(".fa-lock");
+  passwords.forEach((falock) => {
+    falock.addEventListener("click", () => {
+      let password = falock.parentElement.querySelectorAll(".password");
+      password.forEach((password) => {
+        if (password.type == "password") {
+          password.type = "text";
+          return;
+        } else {
+          password.type = "password";
+        }
+      });
+    });
+  });
+}
+
+function mostraLogin() {
+  var form_log = document.querySelector(".login-box");
+  var form_sign = document.querySelector(".signup-box");
+
+  form_log.style.display = "flex";
+  form_sign.style.display = "none";
+}
+function mostraRegister() {
+  var form_log = document.querySelector(".login-box");
+  var form_sign = document.querySelector(".signup-box");
+  form_log.style.display = "none";
+  form_sign.style.display = "flex";
+}
+const LoginForm = document.querySelector("#LoginForm");
+const RegisterForm = document.querySelector("#RegisterForm");
+
+LoginForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const formData = new FormData(LoginForm);
+  const response = await fetch("./login/login.php", {
+    method: "POST",
+    body: formData,
+  });
+  if (response.ok) {
+    const data = await response.json(); // ottieni il contenuto della risposta come oggetto JSON
+    if (data.error) {
+      // gestisci l'errore
+      alert("Error: " + data.error);
+    } else {
+      // la risposta non contiene errori, esegui altre operazioni
+      window.location.href = "YourProfile.php";
+    }
+  } else {
+    // la città o il paese inserito non esiste
+    alert("ERROR RESPONSE");
+  }
+});
+RegisterForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  if (
+    document.getElementById("password").value !==
+    document.getElementById("confirm-password").value
+  ) {
+    alert("PASSWORD NON CONINCIDONO");
+    return;
+  }
+  const formData = new FormData(RegisterForm);
+  const response = await fetch("./register/register.php", {
+    method: "POST",
+    body: formData,
+  });
+  if (response.ok) {
+    const data = await response.json(); // ottieni il contenuto della risposta come oggetto JSON
+    if (data.error) {
+      // gestisci l'errore
+      alert("Error: " + data.error);
+    } else {
+      // la risposta non contiene errori, esegui altre operazioni
+      window.location.href = "YourProfile.php";
+    }
+  } else {
+    // la città o il paese inserito non esiste
+    alert("ERROR RESPONSE");
+  }
+});
