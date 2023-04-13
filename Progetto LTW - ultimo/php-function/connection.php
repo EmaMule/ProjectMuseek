7ffd8c16f772f3dd27294c67b5537f1a9e042da9
palu001 <100961049+palu001@users.noreply.github.com>
2023-04-13 21:27:30 +0200

    Delete Progetto LTW - ultimo directory

--- Progetto LTW - ultimo/php-function/connection.php
+++
@@ -1,4 +0,0 @@
-<?php //ATTENZIONE C'è unA COPIA ESATTA NELLA CARTELLA POST
-$dbconn = pg_connect("host=localhost password=new_password user=postgres port=5432 dbname=MusicNews") or
-    die("Errore di connessione" . pg_last_error());
-?>
