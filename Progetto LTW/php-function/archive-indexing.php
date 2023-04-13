b995f8e6a45deef0a1896f20ec56df76d37c2997
EmaMule <100977128+EmaMule@users.noreply.github.com>
2023-04-13 23:24:03 +0200

    Add files via upload

---
+++ Progetto LTW/php-function/archive-indexing.php
@@ -0,0 +1,74 @@
+<?php
+session_start();
+include './connection.php';
+
+$order = $_POST['direttiva'];
+
+//A SECONDA DELLA DIRETTIVA SPECIFICATA SI CAMBIA L'INDEX
+
+if($order == 'forward'){
+    //PASSIAMO ALLA PROSSIMA PAGINA
+    $_SESSION['indice-paginazione'] += 1;
+
+    //CONTROLLIAMO SE SIAMO ARRIVATI TROPPO LONTANO CON L'INDICE
+    $query_index = "SELECT count(*) as numArticoli from articolo;";
+    $res_index = pg_query($dbconn, $query_index);
+    $line = pg_fetch_array($res_index,null,PGSQL_ASSOC);
+
+    //NON DOVREBBE MAI SUCCEDERE
+    if(!$line){
+        echo json_encode("UNEXPECTED ERROR!");
+        pg_close($dbconn);
+        return;
+    }
+    $num_articoli = intval($line['numarticoli']);
+    $end_index = $num_articoli % 20 === 0 ? $num_articoli / 20 : floor($num_articoli / 20); 
+    if($_SESSION['indice-paginazione'] > $end_index){
+        $_SESSION['indice-paginazione'] -= 1;
+    }
+    
+}
+else if($order == 'backward'){
+    //TORNIAMO ALLA PAGINA PRECEDENTE
+    $_SESSION['indice-paginazione'] -= 1;
+
+    //SE SIAMO TORNATI ALLA PAGINA INIZIALE NON TORNIAMO PIÙ INDIETRO
+    if($_SESSION['indice-paginazione'] == -1){
+        $_SESSION['indice-paginazione'] = 0;
+    }
+}
+else if($order == 'backward-backward'){
+    //TORNIAMO ALLA PAGINA INIZIALE
+    $_SESSION['indice-paginazione'] = 0;
+}
+else if($order == 'forward-forward'){
+    //CALCOLIAMO L'ULTIMO INDICE DISPONIBILE
+    $query_index = "SELECT count(*) as numArticoli from articolo;";
+    $res_index = pg_query($dbconn, $query_index);
+
+    $line = pg_fetch_array($res_index,null,PGSQL_ASSOC);
+
+    //NON DOVREBBE MAI SUCCEDERE
+    if(!$line){
+        echo json_encode("UNEXPECTED ERROR!");
+        pg_close($dbconn);
+        return;
+    }
+    $num_articoli = intval($line['numarticoli']);
+    $index = $num_articoli % 20 === 0 ? $num_articoli / 20 : floor($num_articoli / 20); 
+    $_SESSION['indice-paginazione'] = $index;
+
+
+
+}
+else{
+    //NON DOVREBBE SUCCEDERE MAI
+    echo json_encode("ERROR, INVALID DIRECTIVE!");
+    pg_close($dbconn);
+    return;
+}
+
+echo json_encode("SUCCESS");
+
+pg_close($dbconn);
+?>
